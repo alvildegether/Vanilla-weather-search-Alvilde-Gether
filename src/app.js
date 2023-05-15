d700f351a0103a718b6300be19734418c6c03a65
alvildegether <alvildegether@gmail.com>
2023-05-15 12:51:00 +0200

    fixing bug from earlier
    all code is ok! =)

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ function formatDate(timestamp) {
 }
 
 function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"];
@@ -53,8 +54,9 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = `1d038ee28ef2727a9f0310860ac10ae9`;
-  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat={coordinates.lat}&lon={coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiURL).then(displayForecast);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
